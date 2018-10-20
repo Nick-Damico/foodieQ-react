af2505cbe88538286f3cfacc137fe39613b8fabe
Nick-Damico <nickalan82@icloud.com>
2018-10-19 22:14:39 -0400

    update App.js with rotating background-image, styles updated for .app-title, .app-header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import RecipesContainer from './RecipesContainer';
+import bgOne from './bg-one.jpg';
+import bgTwo from './bg-two.jpg';
+import bgThree from './bg-three.jpg';
+import bgFour from './bg-four.jpg';
+
+const pics = [
+  bgOne,
+  bgTwo,
+  bgThree,
+  bgFour
+];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      background: pics[0]
+    }
+  }
+  componentDidMount() {
+    let i = 1;
+    this.interval = setInterval(() => {
+      console.log(i);
+      this.setState({ background: pics[i] })
+      if(i < pics.length - 1) {
+        i++;
+      } else {
+        i = 0;
+      }
+    }, 4000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
+    const bg = this.state.background;
+    console.log(bg);
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header" style={{backgroundImage: `url(${bg})`}}>
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="app-title">FoodieQ</h1>
         </header>
