3599585b3e8c23d5b632bfd3624009dd9ecfed3c
Nick-Damico <nickalan82@icloud.com>
2018-12-28 17:38:36 -0500

    move App.js to /components, updated import statements to reflect directory change

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import MainHeader from './components/MainHeader';
-import SignupForm from './components/SignupForm';
-import LoginForm from './components/LoginForm';
+import MainHeader from './MainHeader';
+import SignupForm from './SignupForm';
+import LoginForm from './LoginForm';
 
 class App extends Component {
   render() {
