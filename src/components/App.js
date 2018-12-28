023cc529aafc8727272fa4e9255ff9259c4e8a62
Nick-Damico <nickalan82@icloud.com>
2018-12-28 18:06:29 -0500

    import 'react-router-dom', update import statements for components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
 import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
-import SignupForm from './SignupForm';
-import LoginForm from './LoginForm';
+import SignupForm from './users/SignupForm';
+import LoginForm from './users/LoginForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MainHeader />
-        <LoginForm />
+        <BrowserRouter>
+          <div>
+            <MainHeader />
+            <LoginForm />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
