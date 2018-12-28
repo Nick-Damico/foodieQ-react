472921a50bfe6e30c7ca0a9606bdd203c3293c64
Nick-Damico <nickalan82@icloud.com>
2018-12-28 17:15:29 -0500

    Header with scrolling images moved to own component components/MainHeader.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import MainHeader from './components/MainHeader';
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
-import bgOne from './bg-one.jpg';
-import bgTwo from './bg-two.jpg';
-import bgThree from './bg-three.jpg';
-import bgFour from './bg-four.jpg';
-
-const pics = [
-  bgOne,
-  bgTwo,
-  bgThree,
-  bgFour
-];
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      background: pics[0]
-    }
-  }
-  componentDidMount() {
-    let i = 1;
-    this.interval = setInterval(() => {
-      this.setState({ background: pics[i] })
-      if(i < pics.length - 1) {
-        i++;
-      } else {
-        i = 0;
-      }
-    }, 4000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
   render() {
-    const bg = this.state.background;
     return (
       <div className="App">
-        <header className="App-header" style={{backgroundImage: `url(${bg})`}}>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="app-title">FoodieQ</h1>
-        </header>
+        <MainHeader />
         <LoginForm />
       </div>
     );
---
+++ src/components/MainHeader.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import logo from '../images/logo.svg';
+import bgOne from '../images/bg-one.jpg';
+import bgTwo from '../images/bg-two.jpg';
+import bgThree from '../images/bg-three.jpg';
+import bgFour from '../images/bg-four.jpg';
+
+const pics = [
+  bgOne,
+  bgTwo,
+  bgThree,
+  bgFour
+];
+
+export default class MainHeader extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      background: pics[0]
+    }
+  }
+
+  componentDidMount() {
+    let i = 1;
+    this.interval = setInterval(() => {
+      this.setState({ background: pics[i] })
+      if(i < pics.length - 1) {
+        i++;
+      } else {
+        i = 0;
+      }
+    }, 4000);
+  }
+
+  // Might need replaced with componentDidUnmount() later
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const bg = this.state.background;
+    return(
+      <header className="App-header" style={{backgroundImage: `url(${bg})`}}>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="app-title">FoodieQ</h1>
+      </header>
+    )
+  }
+}
