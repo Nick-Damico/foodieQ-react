b45466ee1fb992971e48076030b5d11fcf10513e
Nick-Damico <nickalan82@icloud.com>
2018-12-30 08:21:18 -0500

    import bootstrap styles. organize imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import 'bootstrap/dist/css/bootstrap.css';
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+// Style Imports
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
