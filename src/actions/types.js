27f5d550783de3d8fe172318b26b1993b6127576
Nick-Damico <nickalan82@icloud.com>
2019-01-03 09:55:39 -0500

    simplifying action types to more generic names for reusability of code

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,7 +1,4 @@
 //  Google Auth
-export const GOOGLE_SIGN_IN = "GOOGLE_SIGN_IN";
-export const GOOGLE_SIGN_OUT = "GOOGLE_SIGN_OUT";
-
 export const APP_SIGN_IN = "APP_SIGN_IN";
 export const APP_SIGN_OUT = "APP_SIGN_OUT";
 
