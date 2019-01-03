cb91109dd8c8fdb2e7eebdfa13ffed4ba4f378e0
Nick-Damico <nickalan82@icloud.com>
2019-01-03 12:34:03 -0500

    APP_SIGN_IN_ERROR type for dispatching if Rails Api sign in fails

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,5 +1,6 @@
 //  Google Auth
 export const APP_SIGN_IN = "APP_SIGN_IN";
 export const APP_SIGN_OUT = "APP_SIGN_OUT";
+export const APP_SIGN_IN_ERROR = "APP_SIGN_IN_ERROR";
 
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
