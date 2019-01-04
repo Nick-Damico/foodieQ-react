c15c58afa8064074507284cb4abafda7d21b4217
Nick-Damico <nickalan82@icloud.com>
2019-01-04 17:25:45 -0500

    import FontAwesome library with icons, try to move icon imports to a seperate file on later refactor

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,10 @@ import MainHeader from  "./MainHeader";
 import SignupForm from  "./users/SignupForm";
 import NavigationBar from "./NavigationBar";
 import SiteOverlay from './SiteOverlay';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+library.add(faTimes);
 
 class App extends Component {
   render() {
