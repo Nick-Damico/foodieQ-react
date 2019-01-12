4184abd832c16328b687e4266998a71c0d836bf2
Nick-Damico <nickalan82@icloud.com>
2019-01-11 19:45:49 -0500

    Refactored component to a functional component from a Class component, inlined ActionCreator method calls.

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,62 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import logo from "../images/foodieq-logo.svg";
 import { Navbar, NavbarBrand, Nav, NavItem } from "reactstrap";
 import "./NavigationBar.css";
-import { toggleOverlay, toggleLogInOverlay } from "../actions";
+import {
+  toggleOverlay,
+  toggleLogInOverlay,
+  toggleSignUpOverlay
+} from "../actions";
 
-class NavigationBar extends Component {
-  toggle = () => {
-    this.props.toggleOverlay();
-  };
-
-  toggleLogIn = () => {
-    this.props.toggleLogInOverlay();
-  };
-
-  render() {
-    return (
-      <Navbar color="light" light expand>
-        <div className="hamburger-nav" onClick={this.toggle}>
-          <div className="hamburger-nav__bar1" />
-          <div className="hamburger-nav__bar2" />
-          <div className="hamburger-nav__bar3" />
-        </div>
-        <NavbarBrand>
-          <img className="brand-logo" src={logo} alt="Foodie Q" />
-        </NavbarBrand>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <Link to="/recipes" className="nav-link">
-              Find a Recipe
-            </Link>
-          </NavItem>
-          <NavItem>
-            <Link
-              to="/signup"
-              className="btn nav-link signup-button text-black"
-              onClick={this.toggle}
-            >
-              Sign Up
-            </Link>
-          </NavItem>
-          <NavItem>
-            <Link
-              to="/login"
-              className="btn nav-link login-button text-white"
-              onClick={this.toggleLogIn}
-            >
-              Log In
-            </Link>
-          </NavItem>
-        </Nav>
-      </Navbar>
-    );
-  }
-}
+const NavigationBar = props => {
+  return (
+    <Navbar color="light" light expand>
+      <div className="hamburger-nav" onClick={() => this.props.toggleOverlay()}>
+        <div className="hamburger-nav__bar1" />
+        <div className="hamburger-nav__bar2" />
+        <div className="hamburger-nav__bar3" />
+      </div>
+      <NavbarBrand>
+        <img className="brand-logo" src={logo} alt="Foodie Q" />
+      </NavbarBrand>
+      <Nav className="ml-auto" navbar>
+        <NavItem>
+          <Link to="/recipes" className="nav-link">
+            Find a Recipe
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link
+            to="/signup"
+            className="btn nav-link signup-button text-black"
+            onClick={() => this.props.toggleSignUpOverlay()}
+          >
+            Sign Up
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link
+            to="/login"
+            className="btn nav-link login-button text-white"
+            onClick={() => this.props.toggleLogInOverlay()}
+          >
+            Log In
+          </Link>
+        </NavItem>
+      </Nav>
+    </Navbar>
+  );
+};
 
 export default connect(
   null,
-  { toggleOverlay, toggleLogInOverlay }
+  { toggleOverlay, toggleLogInOverlay, toggleSignUpOverlay }
 )(NavigationBar);
