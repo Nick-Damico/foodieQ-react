db6272bd65a33763eac79b299e27ba1f04edeb18
Nick-Damico <nickalan82@icloud.com>
2019-01-14 21:44:55 -0500

    Refactored Navigation.js to a Class component with (2) helper methods to define the links for logged in User, or a logged out User. This may be separated into seperate child components in the future. Alter .login-button, .signup-button and hover states

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import logo from "../images/foodieq-logo.svg";
@@ -10,17 +10,13 @@ import {
   toggleSignUpOverlay
 } from "../actions";
 
-const NavigationBar = props => {
-  return (
-    <Navbar color="light" light expand>
-      <div className="hamburger-nav" onClick={() => props.toggleOverlay()}>
-        <div className="hamburger-nav__bar1" />
-        <div className="hamburger-nav__bar2" />
-        <div className="hamburger-nav__bar3" />
-      </div>
-      <NavbarBrand>
-        <img className="brand-logo" src={logo} alt="Foodie Q" />
-      </NavbarBrand>
+class NavigationBar extends Component {
+  constructor(props) {
+    super();
+  }
+
+  loggedOutLinks() {
+    return (
       <Nav className="ml-auto" navbar>
         <NavItem>
           <Link to="/recipes" className="nav-link">
@@ -31,7 +27,7 @@ const NavigationBar = props => {
           <Link
             to="/signup"
             className="btn nav-link signup-button text-black"
-            onClick={() => props.toggleSignUpOverlay()}
+            onClick={() => this.props.toggleSignUpOverlay()}
           >
             Sign Up
           </Link>
@@ -40,17 +36,70 @@ const NavigationBar = props => {
           <Link
             to="/login"
             className="btn nav-link login-button text-white"
-            onClick={() => props.toggleLogInOverlay()}
+            onClick={() => this.props.toggleLogInOverlay()}
           >
             Log In
           </Link>
         </NavItem>
       </Nav>
-    </Navbar>
-  );
+    )
+  };
+
+  loggedInLinks() {
+    return (
+      <Nav className="ml-auto" navbar>
+        <NavItem>
+          <Link to="/recipes" className="nav-link">
+            Recipes
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link
+            to="/recipes/new"
+            className="nav-link text-black"
+            onClick={() => this.props.toggleSignUpOverlay()}
+          >
+            Create Recipe
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link
+            to="/blogs"
+            className="nav-link text-black"
+            onClick={() => this.props.toggleLogInOverlay()}
+          >
+            Cooking Blogs
+          </Link>
+        </NavItem>
+      </Nav>
+    );
+  };
+
+  render() {
+    const {isSignedIn} = this.props;
+
+    return (
+      <Navbar color="light" light expand>
+        <div className="hamburger-nav" onClick={() => this.props.toggleOverlay()}>
+          <div className="hamburger-nav__bar1" />
+          <div className="hamburger-nav__bar2" />
+          <div className="hamburger-nav__bar3" />
+        </div>
+        <NavbarBrand>
+          <img className="brand-logo" src={logo} alt="Foodie Q" />
+        </NavbarBrand>
+        {isSignedIn ? this.loggedInLinks() : this.loggedOutLinks()}
+      </Navbar>
+    );
+  };
 };
 
+const mapStateToProps = state => {
+  return {
+    isSignedIn: state.auth.isSignedIn
+  }
+}
 export default connect(
-  null,
+  mapStateToProps,
   { toggleOverlay, toggleLogInOverlay, toggleSignUpOverlay }
 )(NavigationBar);
