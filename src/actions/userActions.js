729d13858673d051cff9d1abbd460a7a7d9a69cb
Nick-Damico <nickalan82@icloud.com>
2019-01-17 14:31:27 -0500

    removed const response variable for storing await axios promise. Using .then and .catch methods for hanlding a successful response or error.

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,14 +1,14 @@
 import axios from "axios";
-import { APP_SIGN_IN, APP_SIGN_IN_ERROR, SET_CURRENT_USER } from "./types";
+import { SET_CURRENT_USER } from "./types";
 // dispatch actions stored in actionHelpers to remove repeated code.
 import { processResponse } from "./actionHelpers";
 
 // Login To Api App, success should dispatch(SIGN_IN)
 export const logInUser = user => {
   return async dispatch => {
-    const response = await axios
+    await axios
       .post("http://localhost:3001/api/v1/auth", { user: user })
-      .then(response => {
+      .then(response => {      
         processResponse(dispatch, response);
       })
       .catch(error => {
@@ -19,7 +19,7 @@ export const logInUser = user => {
 
 export const signUpUser = user => {
   return async dispatch => {
-    const response = await axios
+    await axios
       .post("http://localhost:3001/api/v1/signup", { user: user })
       .then(response => {
         processResponse(dispatch, response);
