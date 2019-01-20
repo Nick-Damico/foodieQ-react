a56a2b06b72907e13172d40cd0ebc0d6caf0fda8
Nick-Damico <nickalan82@icloud.com>
2019-01-20 14:39:49 -0500

    refactor overlay/Login.js and users/LoginForm.js. To make the responsibilities of the components to make more sense where the Login.js is a container which wraps the form and the component LoginForm.js should contain functionality of the form and not worry about the the containers structure.

--- src/components/overlay/Login.js
+++ src/components/overlay/Login.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Greeting from './Greeting';
 import LoginForm from '../users/LoginForm';
-import './Login.css';
+import { Row, Col } from 'reactstrap';
 
-const Login = () => <LoginForm />;
+const Login = props => {
+  return (
+    <Greeting>
+      <Row>
+        <Col
+          xs={{ size: 12 }}
+          sm={{ size: 10, offset: 1 }}
+          md={{ size: 8, offset: 2 }}
+          lg={{ size: 6, offset: 3 }}
+        >
+         <LoginForm />
+        </Col>
+      </Row>
+    </Greeting>
+  );
+};
 
 export default Login;
--- src/components/users/LoginForm.js
+++ src/components/users/LoginForm.js
@@ -1,9 +1,9 @@
+import "./loginForm.css";
 import React, { Component } from "react";
 import Greeting from "../overlay/Greeting";
 import GoogleAuth from "./GoogleAuth";
 import FormErrors from "./FormErrors";
-import { Button, Form, FormGroup, Input, Row, Col } from "reactstrap";
-import "./loginForm.css";
+import { Button, Form, FormGroup, Input } from "reactstrap";
 import { Link } from "react-router-dom";
 import { TweenMax, Power1 } from "gsap/TweenMax";
 import { connect } from "react-redux";
@@ -32,68 +32,53 @@ class LoginForm extends Component {
   render() {
     const { formErrors } = this.props;
     return (
-      <React.Fragment>
-        <Greeting>
-          <Row>
-            {this.props.formErrors.length > 0 ? (
-              <FormErrors errors={formErrors} />
-            ) : null}
-            <Col
-              xs={{ size: 12 }}
-              sm={{ size: 10, offset: 1 }}
-              md={{ size: 8,  offset: 2 }}
-              lg={{ size: 6,  offset: 3 }}
+      <div
+        ref={div => (this.formContainer = div)}
+        className="login-form__container"
+      >
+        {this.props.formErrors.length > 0 ? (
+          <FormErrors errors={formErrors} />
+        ) : null}
+        <Form className="login-form" onSubmit={this.handleOnSubmit}>
+          <FormGroup>
+            <Input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Email"
+              autoComplete="true"
+              value={this.state.email}
+              onChange={e => this.setState({ email: e.target.value })}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              autoComplete="true"
+              value={this.state.password}
+              onChange={e => this.setState({ password: e.target.value })}
+            />
+          </FormGroup>
+          <Button className="login-button" size="lg" block>
+            Login
+          </Button>
+          <GoogleAuth text={"Login with Google"} />
+          <br />
+          Not a member?{" "}
+          <Link to="/signup">
+            <Button
+              onClick={this.props.toggleSignUpOverlay}
+              className="signup-button"
+              size="sm"
             >
-              <div
-                ref={div => (this.formContainer = div)}
-                className="login-form__container"
-              >
-                <Form className="login-form" onSubmit={this.handleOnSubmit}>
-                  <FormGroup>
-                    <Input
-                      id="email"
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      autoComplete="true"
-                      value={this.state.email}
-                      onChange={e => this.setState({ email: e.target.value })}
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Input
-                      id="password"
-                      type="password"
-                      name="password"
-                      placeholder="Password"
-                      autoComplete="true"
-                      value={this.state.password}
-                      onChange={e =>
-                        this.setState({ password: e.target.value })
-                      }
-                    />
-                  </FormGroup>
-                  <Button className="login-button" size="lg" block>
-                    Login
-                  </Button>
-                  <GoogleAuth text={"Login with Google"} />
-                  <br />
-                  Not a member?{" "}
-                  <Link to="/signup">
-                    <Button
-                      onClick={this.props.toggleSignUpOverlay}
-                      className="signup-button"
-                      size="sm"
-                    >
-                      Sign Up
-                    </Button>
-                  </Link>
-                </Form>
-              </div>
-            </Col>
-          </Row>
-        </Greeting>
-      </React.Fragment>
+              Sign Up
+            </Button>
+          </Link>
+        </Form>
+      </div>
     );
   }
 }
