5b3c833d93c7ac892ec5278743f3a62884788043
Nick-Damico <nickalan82@icloud.com>
2019-01-24 09:19:58 -0500

    Additonal div containers added to separate SidePanel.js from the page body. This allows the Page to slide over followed by the SidePanel moving into empty space left by the move. Animation added to page using GSAP library

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,25 @@ import NavigationBar from "./NavigationBar";
 import LandingPage from "./pages/LandingPage";
 import WelcomePage from "./pages/WelcomePage";
 import SiteOverlay from "./overlay/SiteOverlay";
+import SidePanel from "./overlay/SidePanel";
 import CreateRecipe from "./recipes/CreateRecipe";
+import { TweenMax, Back } from "gsap/TweenMax";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { connect } from "react-redux";
 import { jwtLogin } from "../actions";
-import { faTimes, faUserCircle } from "@fortawesome/free-solid-svg-icons";
-library.add(faTimes, faUserCircle);
+import {
+  faTimes,
+  faUserCircle,
+  faBook
+} from "@fortawesome/free-solid-svg-icons";
+library.add(faTimes, faUserCircle, faBook);
 
 class App extends Component {
+  constructor(props) {
+    super();
+
+    this.page = React.createRef();
+  }
   componentDidMount() {
     // If 'foodieq' token is present in localStorage a JWTlogin request is made to Api.
     const token = localStorage.getItem("foodieq-token");
@@ -23,20 +34,37 @@ class App extends Component {
     }
   }
 
+  componentDidUpdate() {
+    if (this.props.isSidePanelOpen) {
+      TweenMax.to(this.page.current, 1, {
+        ease: Back.easeInOut.config(1.5),
+        left: 280
+      });
+    } else {
+      TweenMax.to(this.page.current, 1, {
+        ease: Back.easeInOut.config(1.5),
+        left: 0
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <BrowserRouter>
-          <div>
-            <NavigationBar />
-            {this.props.currentUser.id !== null ?
-              <Route exact path="/" component={WelcomePage} />
-              :
-              <Route exact path="/" component={LandingPage} />
-            }
-            <Route exact path="/recipes/new" component={CreateRecipe} />
-            {this.props.isOverlayOpen ? <SiteOverlay /> : null}
+          <div className="App-inner">
+            <div className="page-wrapper" ref={this.page}>
+              <NavigationBar />
+              {this.props.isSignedIn ? (
+                <Route exact path="/" component={WelcomePage} />
+              ) : (
+                <Route exact path="/" component={LandingPage} />
+              )}
+              <Route exact path="/recipes/new" component={CreateRecipe} />
+              {this.props.isOverlayOpen ? <SiteOverlay /> : null}
+            </div>
           </div>
+          <SidePanel />
         </BrowserRouter>
       </div>
     );
@@ -47,7 +75,7 @@ const mapStateToProps = state => {
   return {
     isOverlayOpen: state.ui.overlay.isOverlayOpen,
     isSignedIn: state.auth.isSignedIn,
-    currentUser: state.currentUser
+    isSidePanelOpen: state.ui.sidePanel.isOpen
   };
 };
 export default connect(
