6d730ea65e563d4af3f49c3da6c3477319063ffc
Nick-Damico <nickalan82@icloud.com>
2019-01-24 09:12:34 -0500

    Add Action Creator for opening and close SidePanel.js with redux state change.

--- src/actions/uiActions.js
+++ src/actions/uiActions.js
@@ -1,11 +1,16 @@
 import {
   TOGGLE_OVERLAY,
   TOGGLE_LOGIN_OVERLAY,
-  TOGGLE_SIGNUP_OVERLAY
+  TOGGLE_SIGNUP_OVERLAY,
+  TOGGLE_SIDE_PANEL
 } from "./types";
 
 export const toggleOverlay = () => {
   return { type: TOGGLE_OVERLAY };
+};
+
+export const toggleSidePanel = () => {
+  return { type: TOGGLE_SIDE_PANEL };
 };
 
 export const toggleLogInOverlay = () => {
