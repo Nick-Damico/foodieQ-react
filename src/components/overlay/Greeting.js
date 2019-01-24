0056d4d764d2d23246926c76779ed5582275528b
Nick-Damico <nickalan82@icloud.com>
2019-01-24 09:13:22 -0500

    Greeting.js takes an argument of 'text' now so the greeting can be customized.

--- src/components/overlay/Greeting.js
+++ src/components/overlay/Greeting.js
@@ -1,15 +1,15 @@
+import "./Greeting.css";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./Greeting.css";
 import { Row, Col } from "reactstrap";
 
-const Greeting = props => {
+const Greeting = (props) => {
   return (
     <React.Fragment>
       <Row className="site-overlay__row-2">
         <Col xs={{ size: 12 }} sm={{ size: 8, offset: 2 }}>
           <div>
-            <h4 className="overlay-header">Welcome to FoodieQ</h4>
+            <h4 className="overlay-header">{props.text}</h4>
             <FontAwesomeIcon icon="user-circle" className="user-icon" />
           </div>
         </Col>
@@ -18,5 +18,9 @@ const Greeting = props => {
     </React.Fragment>
   );
 };
+
+Greeting.defaultProps = {
+  text: 'Welcome to FoodieQ'
+}
 
 export default Greeting;
