be8bd47da085223c578dcb657f393a784b98277c
Nick-Damico <nickalan82@icloud.com>
2019-02-04 17:19:14 -0500

    Wrap Menus.js list items are now wrapped in 'react-router-dom' Link components

--- src/components/sidepanel/Menus.js
+++ src/components/sidepanel/Menus.js
@@ -1,48 +1,85 @@
 import "./Menus.css";
-import React from "react";
-import { Container, Row, Col, Nav, NavItem } from "reactstrap";
+import React, { Component } from "react";
+import { Col, Nav, NavItem } from "reactstrap";
+import { Link } from "react-router-dom";
 import RecipeBookLight from "../../images/sidebar-icons/recipe-book-dark-icon.svg";
 import TypewriterLight from "../../images/sidebar-icons/typewriter-dark-icon.svg";
 import BlogLight from "../../images/sidebar-icons/blog-dark-icon.svg";
 import FoodieqLight from "../../images/sidebar-icons/foodieq-dark-icon.svg";
 
-const Menus = props => {
-  return (
-    <Nav className="menus__nav">
-      <NavItem className="menus__nav-item">
-        <Col xs={{ size: 4 }}>
-          <img className="menus__nav-image" src={RecipeBookLight} alt="Recipe Book Item" />
-        </Col>
-        <Col xs={{ size: 8 }} className="menus__text-container">
-          <p className="menus__text">View Recipes</p>
-        </Col>
-      </NavItem>
-      <NavItem className="menus__nav-item">
-        <Col xs={{ size: 4 }}>
-          <img className="menus__nav-image" src={TypewriterLight} alt="Recipe Book Item" />
-        </Col>
-        <Col xs={{ size: 8 }} className="menus__text-container">
-          <p className="menus__text">Create Recipe</p>
-        </Col>
-      </NavItem>
-      <NavItem className="menus__nav-item">
-        <Col xs={{ size: 4 }}>
-          <img className="menus__nav-image" src={BlogLight} alt="Recipe Book Item" />
-        </Col>
-        <Col xs={{ size: 8 }} className="menus__text-container">
-          <p className="menus__text">View Blogs</p>
-        </Col>
-      </NavItem>
-      <NavItem className="menus__nav-item">
-        <Col xs={{ size: 4 }}>
-          <img className="menus__nav-image" src={FoodieqLight} alt="Recipe Book Item" />
-        </Col>
-        <Col xs={{ size: 8 }} className="menus__text-container">
-          <p className="menus__text">Create Post</p>
-        </Col>
-      </NavItem>
-    </Nav>
-  );
-};
+class Menus extends Component {
+  constructor() {
+    super();
+
+    this.navItems = document.querySelectorAll(".menus__nav-item");
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <Nav className="menus__nav">
+        <NavItem className="menus__nav-item">
+          <Link to="/" className="menus__nav-link">
+            <Col xs={{ size: 4 }}>
+              <img
+                className="menus__nav-image"
+                src={RecipeBookLight}
+                alt="Recipe Book Item"
+              />
+            </Col>
+            <Col xs={{ size: 8 }} className="menus__text-container">
+              <p className="menus__text">View Recipes</p>
+            </Col>
+          </Link>
+        </NavItem>
+        <NavItem className="menus__nav-item">
+          <Link to="/recipes/new" className="menus__nav-link">
+            <Col xs={{ size: 4 }}>
+              <img
+                className="menus__nav-image"
+                src={TypewriterLight}
+                alt="Recipe Book Item"
+              />
+            </Col>
+            <Col xs={{ size: 8 }} className="menus__text-container">
+              <p className="menus__text">Create Recipe</p>
+            </Col>
+          </Link>
+        </NavItem>
+        <NavItem className="menus__nav-item">
+          <Link to="/" className="menus__nav-link">
+            <Col xs={{ size: 4 }}>
+              <img
+                className="menus__nav-image"
+                src={BlogLight}
+                alt="Recipe Book Item"
+              />
+            </Col>
+            <Col xs={{ size: 8 }} className="menus__text-container">
+              <p className="menus__text">View Blogs</p>
+            </Col>
+          </Link>
+        </NavItem>
+        <NavItem className="menus__nav-item">
+          <Link to="/" className="menus__nav-link">
+            <Col xs={{ size: 4 }}>
+              <img
+                className="menus__nav-image"
+                src={FoodieqLight}
+                alt="Recipe Book Item"
+              />
+            </Col>
+            <Col xs={{ size: 8 }} className="menus__text-container">
+              <p className="menus__text">Create Post</p>
+            </Col>
+          </Link>
+        </NavItem>
+      </Nav>
+    );
+  }
+}
 
 export default Menus;
