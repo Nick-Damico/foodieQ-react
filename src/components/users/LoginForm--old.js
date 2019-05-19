596b7c99b858b9ff22e0a7b98431ae79133e69a8
Nick-Damico <nickalan82@icloud.com>
2019-05-19 10:43:11 -0400

    Backup old components, suffix with --old

---
+++ src/components/users/LoginForm--old.js
@@ -0,0 +1,99 @@
+import "./loginForm.css";
+import React, { Component } from "react";
+import GoogleAuth from "./GoogleAuth";
+import FormErrors from "./FormErrors";
+import { Button, Form, FormGroup, Input } from "reactstrap";
+import { Redirect, Link } from "react-router-dom";
+import { TweenMax, Power1 } from "gsap/TweenMax";
+import { connect } from "react-redux";
+import { logInUser, setCurrentUser, toggleSignUpOverlay } from "../../actions";
+
+class LoginForm extends Component {
+  formContainer = null;
+  state = { email: "", password: "" };
+
+  handleOnSubmit = e => {
+    e.preventDefault();
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    this.props.logInUser(user);
+  };
+
+  componentDidMount() {
+    TweenMax.to(this.formContainer, 1.4, {
+      opacity: 1,
+      ease: Power1.easeInOut
+    });
+  }
+
+  render() {
+    const { formErrors } = this.props;
+
+    if (this.props.isSignedIn) {
+      return <Redirect to="/" />
+    }
+    return (
+      <div
+        ref={div => (this.formContainer = div)}
+        className="login-form__container"
+      >
+        {this.props.formErrors.length > 0 ? (
+          <FormErrors errors={formErrors} />
+        ) : null}
+        <Form className="login-form" onSubmit={this.handleOnSubmit}>
+          <FormGroup>
+            <Input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Email"
+              autoComplete="true"
+              value={this.state.email}
+              onChange={e => this.setState({ email: e.target.value })}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              autoComplete="true"
+              value={this.state.password}
+              onChange={e => this.setState({ password: e.target.value })}
+            />
+          </FormGroup>
+          <Button className="login-button" size="lg" block>
+            Login
+          </Button>
+          <GoogleAuth text={"Login with Google"} />
+          <br />
+          Not a member?{" "}
+          <Link to="/signup">
+            <Button
+              onClick={this.props.toggleSignUpOverlay}
+              className="signup-button"
+              size="sm"
+            >
+              Sign Up
+            </Button>
+          </Link>
+        </Form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    formErrors: state.auth.errors,
+    isSignedIn: state.auth.isSignedIn
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { logInUser, setCurrentUser, toggleSignUpOverlay }
+)(LoginForm);
---
+++ src/components/users/SignupForm--old.js
@@ -0,0 +1,112 @@
+import "./SignupForm.css";
+import React, { Component } from "react";
+import FormErrors from "./FormErrors";
+// import GoogleAuth from "./GoogleAuth";
+import { Button, Form, FormGroup, Input, Row, Col } from "reactstrap";
+import { TweenMax, Power1 } from "gsap/TweenMax";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { signUpUser, toggleLogInOverlay } from "../../actions";
+
+class SignUp extends Component {
+  formContainer = null;
+  state = { email: "", password: "", password_confirmation: "" };
+
+  handleOnSubmit = e => {
+    e.preventDefault();
+    let user = {
+      email: this.state.email,
+      password: this.state.password,
+      password_confirmation: this.state.password_confirmation
+    };
+
+    this.props.signUpUser(user);
+  };
+
+  componentDidMount() {
+    TweenMax.to(this.formContainer, 1.4, {
+      opacity: 1,
+      ease: Power1.easeInOut
+    });
+  }
+
+  render() {
+    const { formErrors } = this.props;
+    return (
+      <div
+        ref={div => (this.formContainer = div)}
+        className="signup-form__container"
+      >
+        {formErrors.length > 0 ? <FormErrors errors={formErrors} /> : null}
+        <Form className="login-form" onSubmit={this.handleOnSubmit}>
+          <FormGroup>
+            <Input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Email"
+              autoComplete="true"
+              value={this.state.email}
+              onChange={e => this.setState({ email: e.target.value })}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              autoComplete="new-password"
+              value={this.state.password}
+              onChange={e => this.setState({ password: e.target.value })}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Input
+              id="password_confirmation"
+              type="password"
+              name="password_confirmation"
+              placeholder="Password Confirmation"
+              autoComplete="new-password"
+              value={this.state.password_confirmation}
+              onChange={e =>
+                this.setState({ password_confirmation: e.target.value })
+              }
+            />
+          </FormGroup>
+          <br />
+          <Button className="signup-button" size="lg" block>
+            Sign Up
+          </Button>
+        </Form>
+        <Row>
+          <Col xs={{ size: 12 }} className="google-auth__col">
+            <GoogleAuth text={"Sign Up with Google"} />
+            <br />
+            Already a member?{" "}
+            <Link to="/login">
+              <Button
+                onClick={this.props.toggleLogInOverlay}
+                className="login-button"
+                size="sm"
+              >
+                Login
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    formErrors: state.auth.errors
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { signUpUser, toggleLogInOverlay }
+)(SignUp);
