4311f40cc1ade6b3211c56dda375fadfe934e74e
Nick-Damico <nickalan82@icloud.com>
2019-05-22 20:47:34 -0400

    Format code, remove unused googleSignIn()

--- src/actions/googleActions.js
+++ src/actions/googleActions.js
@@ -1,25 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 import { buildGoogleUser } from "./actionHelpers";
-import { apiUrl } from './api';
-import {
-  SIGN_IN
-} from "./types";
-
-export const googleSignIn = () => {
-  return dispatch => {
-    dispatch({ type: SIGN_IN });
-  };
-};
+import { apiUrl } from "./api";
+import { SIGN_IN } from "./types";
 
 export const logInGoogleUser = user => {
   const googleUser = buildGoogleUser(user);
-  return async dispatch => {
-    const response = await axios.post(`${apiUrl}/auth/google`, {
-      user: { email: googleUser["data"]["user"]["email"] }
-    });
-    if (response.status === 200) {
-      localStorage.setItem("foodieq-token", response.data.token);
-      dispatch({ type: SIGN_IN, payload: googleUser });
-    }
+  return dispatch => {
+    axios
+      .post(`${apiUrl}/auth/google`, {
+        user: { email: googleUser["data"]["user"]["email"] }
+      })
+      .then(response => {
+        localStorage.setItem("foodieq-token", response.data.token);
+        dispatch({ type: SIGN_IN, payload: googleUser });
+      });
   };
 };
